b923ec40d5b1fa7f69091cbd33835297519fa50b
vijay singh bisht <58104346+vijay-singh07@users.noreply.github.com>
2023-10-12 12:41:07 +0530

    Update mongodb.js

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 
 export const connectMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URL);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log("Error connecting to MongoDB: ", error.message);
